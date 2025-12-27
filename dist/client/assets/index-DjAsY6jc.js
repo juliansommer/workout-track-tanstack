b4c7e98c78c94d5cd06e6286828983938d72b00f
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-12-28 09:32:31 +1100

    Remove dist directory from version control

--- dist/client/assets/index-DjAsY6jc.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-DfuXOvW1.js";function m(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!!!"})})}export{m as component};
--- dist/server/assets/_tanstack-start-manifest_v-jWxOkcgJ.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "C:/Users/Julian/Desktop/workout-track-tanstack/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-DfuXOvW1.js"], "assets": [] }, "/": { "filePath": "C:/Users/Julian/Desktop/workout-track-tanstack/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-DjAsY6jc.js"] } }, "clientEntry": "/assets/main-DfuXOvW1.js" });
-export {
-  tsrStartManifest
-};
